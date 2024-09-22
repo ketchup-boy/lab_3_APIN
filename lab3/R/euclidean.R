ade3401bc8e363f0a734db7d1073255bde6de2dc
Ana-S-Work <anastanojcic03@gmail.com>
2024-09-22 16:03:00 +0200

    description

--- lab3/R/euclidean.R
+++ lab3/R/euclidean.R
@@ -1,3 +1,12 @@
+#'Illustration of the Euclidean algorithm that is finding the greatest common denominator between two numbers
+#'
+#' @param a Numeric value that is bigger than b
+#' @param b Numeric value that is smaller than a
+#' @return greatest common divisor of numbers a and b is found
+#' @references https://en.wikipedia.org/wiki/Euclidean_algorithm
+
+
+
 euclidean <- function(a, b) {
   while(b != 0){
     n <- b
